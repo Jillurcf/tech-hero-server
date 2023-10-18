d86eaa24dfdba1ccaad03bd83e4bf6f018a1f9fc
jillurcf <jrtushar@gmail.comm>
2023-10-17 21:43:27 -0700

    send data to database by POST method successfully

--- index.js
+++ index.js
@@ -29,7 +29,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const addedProductCollection = client.db("addedprodDB").collection('addedproduct')
+
+    app.get('/addedproduct', async(req, res)=>{
+      const cursor = addedProductCollection.find()
+      const result =await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/addedproduct', async(req, res)=>{
+      const addedProduct = req.body;
+      const result = await addedProductCollection.insertOne(addedProduct)
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
